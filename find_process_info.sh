6dc52e8e976a56797bd6a47088a7b7e77a0b6171
Fabio Ticconi <fabio.ticconi@gmail.com>
2014-05-10 19:39:43 +0100

    changed tabs with spaces

--- find_process_info.sh
+++ find_process_info.sh
@@ -10,11 +10,11 @@
 
 if [ ! -n "$1" ]
 then
-	echo "Usage: `basename $0` <process name>"
-	exit $E_BADARGS
+    echo "Usage: `basename $0` <process name>"
+    exit $E_BADARGS
 fi
 
 for i in `pidof $1`
 do
-	lsof -p $i | head -n 2 | tail -n 1 | awk '{print $2 " " $9}'
+    lsof -p $i | head -n 2 | tail -n 1 | awk '{print $2 " " $9}'
 done
